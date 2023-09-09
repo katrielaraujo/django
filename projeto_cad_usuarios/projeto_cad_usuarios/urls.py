8e726dbbb023d1fe6d23872c2d469caff0f0cbbd
Katriel Araújo <53996261+katrielaraujo@users.noreply.github.com>
2023-09-09 17:45:00 -0300

    Tela de cadastro de usuarios

---
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from app_cad_usuarios import views
+
+urlpatterns = [
+    # rota, view responsavel, nome de referencia
+    path('',views.home,name='home'),
+    path('usuarios/',views.usuarios,name='listagem_usuarios')
+]
